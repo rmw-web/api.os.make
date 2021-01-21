dca90910138440cff4298017b1abee7029573203
gcxfd <i@rmw.link>
2021-01-21 22:52:20 +0800

    master

--- openresty.sh
+++ openresty.sh
@@ -14,9 +14,10 @@ if ! hash openssl 2>/dev/null; then
 brew install openssl
 fi
 
-fi
+ssldir=/usr/local/Cellar/openssl
+sslver=$(ls /usr/local/Cellar/openssl@*/|sort|tail -1)
 
-brew install pcre openssl
+fi
 
 _DIR=$(dirname $(realpath "$0"))
 
@@ -26,19 +27,25 @@ mkdir -p $tmp
 
 cd $tmp
 
+openssldir=openssl-1.1.1i
+if [[ ! -d "$openssldir" ]] ; then
+wget -c https://www.openssl.org/source/$openssldir.tar.gz
+tar zxcf $openssldir.tar.gz
+fi
+
 version=1.19.3.1
 tardir=openresty-$version
 tarname=$tardir.tar.gz
+if [[ ! -d "$tardir" ]] ; then
 wget -c https://openresty.org/download/$tarname
-
-
 tar zxvf $tarname
+fi
 
 cd $tardir
 
+./configure --prefix=/opt/openresty --with-cc-opt="-I/usr/local/include" --with-luajit --without-http_redis2_module --with-ld-opt="-L/usr/local/lib" --with-openssl="$tmp/$openssldir"
 
-# make
-#
+make
 
 # mv src/redis-server $_DIR/../os.$os/redis-server.exe
 #
