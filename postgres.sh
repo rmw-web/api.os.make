138ffb5f820a17d3f9b2aecb4c62d7254a7e3159
gcxfd <i@rmw.link>
2021-01-22 11:10:54 +0800

    master

--- postgres.sh
+++ postgres.sh
@@ -26,29 +26,7 @@ unzip postgres.zip
 mkdir -p postgres
 tar -xvf postgres-*.txz -C ./postgres
 
-# openssldir=openssl-1.1.1i
-# if [[ ! -d "$openssldir" ]] ; then
-# openssltgz=$openssldir.tar.gz
-# wget -c https://www.openssl.org/source/$openssltgz
-# tar zxcf $openssltgz
-# rm -rf $openssltgz
-# fi
-#
-# version=1.19.3.1
-# xdir=openresty-$version
-# tgz=$xdir.tar.gz
-# if [[ ! -d "$xdir" ]] ; then
-# wget -c https://openresty.org/download/$tgz
-# tar zxvf $tgz
-# rm -rf $tgz
-# fi
-#
-# cd $xdir
-# installdir=$tmp/openresty
-# mkdir -p $installdir
-# ./configure --prefix=$installdir --with-cc-opt="-I/usr/local/include" --with-luajit --without-http_redis2_module --with-ld-opt="-L/usr/local/lib" --with-openssl="$tmp/$openssldir"
-#
-# make
-# make install
-# cp $installdir/nginx/sbin/nginx $_DIR/../os.$os/openresty.exe
-# cd $tmp && rm -rf $openssldir && rm -rf $xdir && rm -rf $installdir
+osdir=$_DIR/../os.$os
+rm -rf $osdir/postgres
+mv postgres $osdir
+rm -rf $tmp
